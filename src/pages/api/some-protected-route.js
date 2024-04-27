ed1c91a28f100bb5360e796c8f9fcc9b3201ee9a
AngusBlomley <125128100+AngusBlomley@users.noreply.github.com>
2024-04-27 20:11:07 +0100

    added dbConnect Module and setup mongo data base

--- src/pages/api/some-protected-route.js
+++ src/pages/api/some-protected-route.js
@@ -1,6 +1,8 @@
 import auth from '../../middleware/auth';
+import dbConnect from '../../lib/dbConnect';
 
 export default async function handler(req, res) {
+    await dbConnect();
     auth(req, res, () => {
         // Protected route logic here
         res.status(200).json({ message: 'You are authenticated to see this.' });
